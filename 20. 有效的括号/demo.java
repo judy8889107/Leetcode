776769cc242269780632f487f4a9192096956abf
judy <judy8889107@gmail.com>
2022-10-30 16:06:48 +0800

    20. 有效的括号

---
+++ 20. 有效的括号/demo.java
@@ -0,0 +1,59 @@
+import java.util.*;
+import java.lang.*;
+/**
+ * 這題有看提示
+ * stack存放open bracket，內層的放在最上面
+ * 檢查close bracket，若符合則remove stack top
+ * 否則返回false
+ */
+class Solution {
+    public boolean isValid(String s) {
+        if(s.length()%2 != 0) return false;
+        boolean isValid = true;
+        ArrayList<Character> stack = new ArrayList<>();
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i) == '(' || s.charAt(i) == '[' || s.charAt(i) == '{'){
+                stack.add(0,s.charAt(i));
+            }
+            if(stack.size()!=0){
+                switch(s.charAt(i)){
+                    case ')':
+                        if(stack.get(0) != '(') return false;
+                        else stack.remove(0);
+                    break;
+                    case ']':
+                        if(stack.get(0) != '[') return false;
+                        else stack.remove(0);
+                    break;
+                    case '}':
+                        if(stack.get(0) != '{') return false;
+                        else stack.remove(0);
+                    break;
+                }
+
+            }else return false;
+            
+            
+        }
+        if(stack.size() == 0) return true;
+        else return false;
+
+    }
+}
+
+public class demo {
+
+    public static void main(String[] args) {
+
+        Solution solution = new Solution();
+        Scanner sc = new Scanner(System.in);
+        String line;
+        while (sc.hasNextLine()) {
+            line = sc.nextLine();
+            System.out.println(line);
+            System.out.printf("isValid: %b\n",solution.isValid(line));
+
+        }
+
+    }
+}
