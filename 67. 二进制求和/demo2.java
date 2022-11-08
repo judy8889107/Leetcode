8f74ebd919183a637b4c2748d7cef34500b2aff2
judy <judy8889107@gmail.com>
2022-11-08 08:32:19 +0800

    67. 二进制求和(增加題解)

--- 67. 二进制求和/demo2.java
+++ 67. 二进制求和/demo2.java
@@ -2,6 +2,14 @@ import java.util.*;
 /**
  * 执行用时：1 ms,    在所有 Java 提交中击败了 100.00% 的用户
  * 内存消耗：39.9 MB, 在所有 Java 提交中击败了 90.45%  的用户
+ * 題解：
+ * 將較大string assign給a, 小的就是b, 若相等就沒差
+ * 將答案(result)設定為比較大的那個string, 先讓那個string 轉為 char array
+ * 然後先看有無carry 決定 result 值的變化
+ * 後面將result和 min string 相加, carry的判斷在前面carry有做了
+ * 這裡特別注意，也許剛好carry, carry+result[i] = 0, 又是carry, 那下面跟min string相加不會改變carry的值
+ * 下面唯一設定carry為true的情況只有11
+ * 其他01,10,00,都不能確定,有可能0是因為 carry+result[i] = 0, 0 在和minstr[j]相加, 這時候就要沿用carray值
  */
 class Solution {
     public String addBinary(String a, String b) {
