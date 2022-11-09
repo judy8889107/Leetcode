519af946fb0d4db67e08e97afc83acc551109fb6
judy <judy8889107@gmail.com>
2022-11-09 19:05:54 +0800

    70. 爬楼梯 [小改，但內存消耗還是不理想]

--- 70. 爬楼梯/demo.java
+++ 70. 爬楼梯/demo.java
@@ -1,24 +1,24 @@
-import java.math.BigInteger;
+import java.math.BigDecimal;
 import java.util.*;
 /**
- * 执行用时：4 ms,    在所有 Java 提交中击败了 100.00% 的用户
- * 内存消耗：40.4 MB, 在所有 Java 提交中击败了 5.13%   的用户
+ * 执行用时：9 ms,    在所有 Java 提交中击败了 100.00% 的用户
+ * 内存消耗：41.2 MB, 在所有 Java 提交中击败了 5.15%   的用户
  */
 class Solution {
     public int climbStairs(int n) {
 
-        int two_nums = n/2;
+        int two_nums = n / 2;
         int count = n;
-        for(int i=2; i<=two_nums;i++ ){
-            BigInteger top = new BigInteger("1");
-            BigInteger down = new BigInteger("1");
-            
-           for(int j=n-i, k=1;k<=i;k++,j--){
-            top = top.multiply(new BigInteger(Integer.toString(j)));
-            down = down.multiply(new BigInteger(Integer.toString(k)));
-            
-           }
-           count += top.divide(down).intValue();
+        for (int i = 2; i <= two_nums; i++) {
+
+            BigDecimal tmp = new BigDecimal("1.0");
+
+            for (int j = n - i, k = 1; k <= i; k++, j--) {
+                tmp = tmp.multiply(new BigDecimal(Double.toString(j)));
+                tmp = tmp.divide(new BigDecimal(Double.toString(k)));
+            }
+
+            count += tmp.intValue();
 
         }
 
@@ -26,17 +26,16 @@ class Solution {
     }
 }
 
-
-public class demo{
+public class demo {
     public static void main(String[] args) {
 
         Solution solution = new Solution();
         Scanner sc = new Scanner(System.in);
         int n;
-        while(sc.hasNext()){
+        while (sc.hasNext()) {
             n = sc.nextInt();
-            solution.climbStairs(n);
+            System.out.printf("n: %d\nans: %d\n", n, solution.climbStairs(n));
         }
-        
+
     }
 }
